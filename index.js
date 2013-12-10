1a939650c8ec4d418bdd76fc572a064e1bc558ed
Leo Cassarani <leo.cassarani@me.com>
2013-12-10 17:50:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require("http"),
+    request = require("request");
+
+function fetchExchangeRate(currency, success) {
+  request.get("http://blockchain.info/ticker", function(error, response, body) {
+    if (response.statusCode == 200) {
+      var rates = JSON.parse(body),
+          rate = rates[currency]['15m'];
+
+      success(rate);
+    }
+  });
+}
+
+http.createServer(function(request, response) {
+  fetchExchangeRate("USD", function(rate) {
+    var payload = { item: [{ value: rate, prefix: "$" }] };
+
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify(payload));
+    response.end();
+  });
+}).listen(80);
